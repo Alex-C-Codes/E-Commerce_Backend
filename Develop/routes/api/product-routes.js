ce28e605d7349d9bccc30d9a2596af703008b467
Alex-C <alexcook1613@gmail.com>
2022-12-28 20:32:47 -0500

    finished models. Finished routes. Finished server.js. Seeded the database with test data

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,18 +4,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findAll({
+      include: [Category, { model: Tag, through: ProductTag }]
+    });
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  } 
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const products = await Product.findByPk(req.params.id, {
+      include: [Category, { model: Tag, through: ProductTag }]
+    });
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  } 
 });
 
-// create new product
+// create new product DONE
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
@@ -47,7 +63,7 @@ router.post('/', (req, res) => {
     });
 });
 
-// update product
+// update product DONE
 router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
@@ -89,8 +105,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const products = await Product.destroy({
+      where: { id: req.params.id }
+    });
+    if (!products) {
+      res.status(404).json({ message: 'No category wit this id!' });
+      return;
+    }
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+const routes = require('./routes')
+const sequelize = require('./config/connection');
+// const { Sequelize } = require('sequelize');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// app.listen(PORT, () => {
+//   console.log(`App listening on port ${PORT}!`);
+// });
+
+// [ ] look into sequelize.sync force: true
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
 });
